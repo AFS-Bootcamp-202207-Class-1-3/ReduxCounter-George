c317caabda9d494d439ec3e7fb99b51264c7d13f
CHENGE8 <1911270452@qq.com>
2022-08-01 17:13:21 +0800

    feat:implement multiple counter

--- src/components/CounterGroup.jsx
+++ src/components/CounterGroup.jsx
@@ -1,11 +1,12 @@
 import Counter from "./Counter";
 
-function CounterGroup() {
+function CounterGroup(props) {
+    const {counterSize} = props
+    const counters = new Array(parseInt(counterSize)).fill(1).map((element, index) => (<Counter key={index}/>))
+
     return(
         <div className="couter-group">
-            <Counter/>
-            <Counter/>
-            <Counter/>
+           {counters}
         </div>
     )
 }
--- src/components/MultipleCounter.jsx
+++ src/components/MultipleCounter.jsx
@@ -1,3 +1,18 @@
+import CounterGroup from "./CounterGroup";
+import CounterSizeGenerator from "./CounterSizeGenerator";
+import {useState} from "react";
 function MultipleCounter() {
-    
+    const [counterSize, setCounterSzie] = useState(0)
+
+    const updateCounterSize = (size) => {
+        setCounterSzie(size)
+    }
+    return(
+        <div className="multiple-counter">
+            <CounterSizeGenerator updateCounterSize={updateCounterSize}/>
+            <CounterGroup counterSize={counterSize}/>
+        </div>
+    )
 }
+
+export default MultipleCounter
