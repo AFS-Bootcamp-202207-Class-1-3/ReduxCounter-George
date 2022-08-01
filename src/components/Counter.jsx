8a31064cec8c67ef47be8d4be0e5271949dfaa45
CHENGE8 <1911270452@qq.com>
2022-08-01 15:47:22 +0800

    feat：implement Counter

---
+++ src/components/Counter.jsx
@@ -0,0 +1,21 @@
+import { useState } from "react";
+import "./Counter.css";
+function Counter() {
+  const [count, setCount] = useState(0);
+  const increase = () => {
+    setCount(count + 1);
+  };
+
+  const decrease = () => {
+    setCount(count - 1);
+  };
+  return (
+    <div >
+      <button className="button" onClick={increase}>+</button>
+      <span>{count}</span>
+      <button onClick={decrease}>-</button>
+    </div>
+  );
+}
+
+export default Counter;
